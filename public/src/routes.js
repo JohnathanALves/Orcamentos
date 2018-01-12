52569940e61d1713cb343f9303d78cf26a2ea6f7
Administrator@PLANTEBEM.COM <Administrator@PLANTEBEM.COM>
2018-01-12 10:32:38 -0300

    separacao do front

--- public/src/routes.js
+++
@@ -1,8 +0,0 @@
-import Home from './Home.vue';
-import Cotacao from './Cotacao.vue';
-
-export const routes = [
-    { name: 'home', path: '', component: Home, titulo: 'Home' },
-    { name: 'cotacaoNova', path: '/cotacao', component: Cotacao, titulo: 'Cotacao' },
-    { name: 'cotacaoExistente', path: '/cotacao/:id', component: Cotacao, titulo: 'Cotacao', props: true },
-];
--- routes/orcamento.js
+++ routes/orcamento.js
@@ -3,50 +3,50 @@ var router = express.Router();
 var Moment = require('moment');
 var Controller = require('../controllers/orcamentoController');
 
-router.get('/', function(req, res, next) {
-    Controller.getOrcamentos(function(result) {
+router.get('/', function (req, res, next) {
+    Controller.getOrcamentos(function (result) {
         return res.json(result);
     });
 });
 
-router.get('/report', function(req, res, next) {
-    Controller.report(function(result) {
+router.get('/report', function (req, res, next) {
+    Controller.report(function (result) {
         res.download(result, 'relatorio.csv');
     });
 });
 
-router.get('/:id', function(req, res, next) { //orcamento por id
+router.get('/:id', function (req, res, next) { //orcamento por id
     var orcamento = req.params.id;
-    Controller.getOrcamentosByID(orcamento, function(orc) {
+    Controller.getOrcamentosByID(orcamento, function (orc) {
         console.log("Get Orcamento By ID: ");
         console.log(orc);
         return res.json(orc);
     });
 });
 
-router.get('/data/:dtIn/:dtFim', function(req, res, next) {
+router.get('/data/:dtIn/:dtFim', function (req, res, next) {
     var dtIn = new Moment(req.params.dtIN);
     var dtFim = new Moment(req.params.dtFim);
     console.log(dtIn);
     console.log(dtFim);
-    Controller.getOrcamentosByDate(dtIn, dtFim, function(resultado) {
+    Controller.getOrcamentosByDate(dtIn, dtFim, function (resultado) {
         console.log(resultado);
         return res.json(resultado);
     });
 });
 
-router.post('/:id', function(req, res, next) { //novo orcamento
+router.post('/:id', function (req, res, next) { //novo orcamento
     console.log('cadastro de novo orcamento');
     var data = new Moment(new Date(req.body.data))
     var parceiro = req.body.parceiro;
     var vendedor = req.params.id;
-    Controller.createOrcamento(data, parceiro, vendedor, function(orc) {
+    Controller.createOrcamento(data, parceiro, vendedor, function (orc) {
         console.log(orc);
         return res.json(orc);
     });
 });
 
-router.put('/:id/:cod', function(req, res, next) { //atualiza dados de orcamento
+router.put('/:id/:cod', function (req, res, next) { //atualiza dados de orcamento
     let vendedor = req.params.id;
     let codOrcamento = req.params.cod;
     let itens = req.body.itens;
@@ -59,21 +59,21 @@ router.put('/:id/:cod', function(req, res, next) { //atualiza dados de orcamento
     return res.json(response);
 });
 
-router.delete('/:cod', function(req, res, next) {
+router.delete('/:cod', function (req, res, next) {
     let id = req.params.cod;
     console.log('Delete Orcamento by ID: ', id);
     let response = Controller.deleteByID(id);
     return res.json(response);
 });
 
-router.post('/:id/:cod', function(req, res, next) { //insere produto no orcamento
+router.post('/:id/:cod', function (req, res, next) { //insere produto no orcamento
     var vendedor = req.params.id;
     var codOrcamento = req.params.cod;
     var codProduto = req.body.codProduto;
     var quantidade = req.body.quantidade;
     var valor = req.body.valor;
 
-    Controller.insertProduto(codOrcamento, codProduto, quantidade, valor, function(orcamento) {
+    Controller.insertProduto(codOrcamento, codProduto, quantidade, valor, function (orcamento) {
         console.log(orcamento);
         return res.json(orcamento);
     });
